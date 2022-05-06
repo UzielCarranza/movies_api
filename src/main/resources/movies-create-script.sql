ba4c0bca2f14124fc190f2e81acf8a885ea558f7
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-06 13:18:35 -0400

    added actors and genres joins

--- src/main/resources/movies-create-script.sql
+++ src/main/resources/movies-create-script.sql
@@ -15,11 +15,12 @@ USE movies_db;
 
 # 5. drop the table(s) to which no other tables are dependent (none at first)
 
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS movie_genre;
+DROP TABLE IF EXISTS movie_actor;
 DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS actors;
-
+DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS movies;
 
 # 6. map the json movie properties to movies table columns
 # --> start with just a movies table with all the columns found in the movie json properties
@@ -54,6 +55,7 @@ CREATE TABLE IF NOT EXISTS actors
 );
 DESCRIBE actors;
 
+
 # movies tables
 CREATE TABLE IF NOT EXISTS movies
 (
@@ -65,14 +67,34 @@ CREATE TABLE IF NOT EXISTS movies
     rating      CHAR(1),
     director_id INT UNSIGNED NOT NULL,
     genre_id    INT UNSIGNED NOT NULL,
-    actors_id   INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (director_id) REFERENCES directors (id),
-    FOREIGN KEY (genre_id) REFERENCES genre (id),
-    FOREIGN KEY (actors_id) REFERENCES actors (id)
+    actor_id    INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id)
+#     FOREIGN KEY (director_id) REFERENCES directors (id),
+#     FOREIGN KEY (genre_id) REFERENCES genre (id),
+#     FOREIGN KEY (actor_id) REFERENCES actors (id)
 );
 
 DESCRIBE movies;
+
+
+CREATE TABLE IF NOT EXISTS movie_genre
+(
+    movie_id INT UNSIGNED NOT NULL,
+    genre_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (genre_id) REFERENCES genre (id)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS movie_actor
+(
+    movie_id INT UNSIGNED NOT NULL,
+    actor_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies (id),
+    FOREIGN KEY (actor_id) REFERENCES actors (id)
+);
+
 
 # 6a. Run the script to make sure it works
 
