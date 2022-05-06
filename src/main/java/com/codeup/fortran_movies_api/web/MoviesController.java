4b6ca59a57a90c08a1b4563eda78af90bf572c27
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-06 19:18:13 -0400

    introduced two request mapping for one and many movies

--- src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
+++ src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
@@ -6,14 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/movies")
 public class MoviesController {
 
+
+    @RequestMapping("/api/movie")
     @GetMapping
     public Movie one() {
         return new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
     }
+
+
+    @RequestMapping("/api/movies")
+    @GetMapping
+    public List<Movie> getAll() {
+        List<Movie> Movies = new ArrayList<>();
+        Movie movie1 = new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
+        Movie movie2 = new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
+        Movies.add(movie1);
+        Movies.add(movie2);
+        return Movies;
+    }
+
+
 }
