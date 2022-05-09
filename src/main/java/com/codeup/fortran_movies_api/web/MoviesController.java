f1328922b84944132201435f441801d144a457ff
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-09 09:32:41 -0400

    returning a single movie based on an id

--- src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
+++ src/main/java/com/codeup/fortran_movies_api/web/MoviesController.java
@@ -1,10 +1,7 @@
 package com.codeup.fortran_movies_api.web;
 
 import com.codeup.fortran_movies_api.data.Movie;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,24 +11,35 @@ import java.util.List;
 @RestController
 public class MoviesController {
 
-
-    @RequestMapping("/api/movie")
-    @GetMapping
+    @RequestMapping("/api/movie/{id}")
+    @GetMapping("id")
     public Movie one() {
         return new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
     }
 
 
     @RequestMapping("/api/movies")
-    @GetMapping
+    @GetMapping()
     public List<Movie> getAll() {
         List<Movie> Movies = new ArrayList<>();
         Movie movie1 = new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
-        Movie movie2 = new Movie(1, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
+        Movie movie2 = new Movie(2, "test", "1995", "someone", "The dude", "idk", "comedy", "some plot", "assaas");
         Movies.add(movie1);
         Movies.add(movie2);
         return Movies;
     }
 
-
+//    @RequestMapping("/api/movies/{id}")
+//    @GetMapping()
+//    public Movie getById(@PathVariable Long id) {
+//        Movie movie;
+//        List<Movie> movieList = getAll();
+//        for (Long i = id; i < movieList.size(); i++) {
+//            if (movieList.contains(getAll().get(Math.toIntExact(id)))) {
+//                movie = movieList.forEach(movieList.);
+//                return movie;
+//            }
+//        }
+//        return movie;
+//    }
 }
